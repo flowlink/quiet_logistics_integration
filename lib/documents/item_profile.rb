9297358c4dbb6fe726df9938f3389f9da1a77414
Raymond Sapida <ray.sapida@gmail.com>
2020-08-10 18:04:56 -0700

    Refactor a little and standardize field keys

--- lib/documents/item_profile.rb
+++ lib/documents/item_profile.rb
@@ -15,26 +15,31 @@ module Documents
         'BusinessUnit' => @unit,
         'ItemNo' => @item['sku'],
         'UPCno' => @item['upc'] || @item['sku'],
-        'StockWgt' => "1.0000",
-        'StockUOM' => "EA",
+        'StockWgt' => @item['stock_weight'] || "1.0000",
+        'StockUOM' => @item['stock_uom'] || "EA",
         'ItemDesc' => @item['description'],
         'ImageUrl' => @item['image_url'],
         'ItemSize' => @item['item_size'],
-        'ItemMaterial' => @item['material'],
-        'ItemColor' => @item['color_name'],
+        'ItemMaterial' => @item['item_material'],
+        'ItemColor' => @item['item_color'],
         'CommodityClass' => @item['commodity_class'],
-        'CommodityDesc' => @item['description'],
+        'CommodityDesc' => @item['commodity_desc'],
         'VendorName' => @item['vendor_name'],
         'VendorItemNo' => @item['vendor_item_no']
       }.compact
+
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.ItemProfileDocument('xmlns' => 'http://schemas.quietlogistics.com/V2/ItemProfile.xsd') {
           xml.ItemProfile(item_profile) {
-            xml.UnitQuantity('BarCode' => (@item['upc'] || @item['sku']), 'Quantity' => (@item['quantity'] || '1'), 'UnitOfMeasure' => 'EA')
+            xml.UnitQuantity('BarCode' => determine_barcode, 'Quantity' => (@item['quantity'] || '1'), 'UnitOfMeasure' => 'EA')
           }
         }
       end
       builder.to_xml
+    end
+
+    def determine_barcode
+      @item['barcode'] || @item['upc'] || @item['sku']
     end
 
     def date_stamp
